d913bc8eec40309ce6a501cff870837d5a88fa95
Comhk <com-hk@hotmail.com>
2019-03-21 19:00:25 +0800

    Support api record query
    Support query parameter 'from' and 'to'.

--- Sources/App/Controllers/RecordsController.swift
+++ Sources/App/Controllers/RecordsController.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Fluent
 
 struct RecordsController: RouteCollection {
     func boot(router: Router) throws {
@@ -16,7 +17,26 @@ struct RecordsController: RouteCollection {
     }
 
     func getAllHandler(_ req: Request) throws -> Future<[Record]> {
-        return Record.query(on: req).all()
+        var builder = Record.query(on: req)
+        if let from = req.query[String.self, at: "from"] {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+            guard let _ = formatter.date(from: from) else {
+                print("Incorrect date format: from: " + from)
+                throw Abort(.badRequest)
+            }
+            builder = builder.filter(\.recordTime >= from)
+        }
+        if let to = req.query[String.self, at: "to"] {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+            guard let _ = formatter.date(from: to) else {
+                print("Incorrect date format: to: " + to)
+                throw Abort(.badRequest)
+            }
+            builder = builder.filter(\.recordTime <= to)
+        }
+        return builder.sort(\.recordTime, .ascending).all()
     }
 
     func getHandler(_ req: Request) throws -> Future<Record> {
